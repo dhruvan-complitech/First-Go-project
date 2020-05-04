9592ece80ce263900986b8c7d57d1ef93dda28b7
Dhruvan Bhalara <dhruvan@complitech.net>
2020-05-04 11:57:03 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Print The statement
+	fmt.Println("Hello There!")
+
+	//long way to declare variable
+	//var name string = "No Name"
+
+	//Sort way to declare variable
+	name := "No Name"
+	fmt.Println(name)
+	//Reassign variable
+	name = "We All Have Name"
+	fmt.Println(name)
+}
